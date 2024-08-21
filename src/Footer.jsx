506d0e933deea17db3170c8774f7d68e52f129aa
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-21 21:38:01 +0300

    footer added style

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,7 @@
-
-import React from 'react'
+import React from "react";
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+  return <div style={{ backgroundColor: "lavender" }}>Footer</div>;
 }
 
-export default Footer
+export default Footer;
