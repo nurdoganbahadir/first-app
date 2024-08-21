950c8a0f9aa48eafa03afa762362fcf8f588354d
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-21 21:39:05 +0300

    Main.jsx import index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Footer from "./Footer";
+import Main from "./Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Main />
     <Footer />
   </React.StrictMode>
 );
